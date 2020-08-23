18834d3160c1d17363731f9cbec7cb6ee9fd6f59
plasmatech8 <com0004@gmail.com>
2020-08-23 14:35:52 +1000

    init corey js tutorial

--- javascript-essentials-training/02_basics/index.js
+++
@@ -1,6 +0,0 @@
-
-var date = new Date();
-document.body.innerHTML = "<h1>Today is " + date.getDate() + "/" + (date.getMonth() + 1) + '/' + date.getFullYear() + "</h1>";
-
-console.log("index.js has been executed!");
-alert("index.js has been executed!");
